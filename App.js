99d8d7589d7aea6d1f2321665a48650d8aade8e9
David Dahlman <daviddahlman82@gmail.com>
2021-11-02 14:37:44 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Pressable } from 'react-native';
 export default function App() {
+  
+  const [isPressed, setIsPressed] = useState(false)
+  const [showPassword, setShowPassword] = useState(true)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.usernamePasswordContainer}>
+        <View style={styles.usernameContainer}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput style={styles.username}/>  
+        </View>
+        <View style={styles.passwordContainer}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput style={styles.password} secureTextEntry={showPassword}/>
+          <Pressable   onPress={() =>  setShowPassword(!showPassword)}>
+         <View style={isPressed? [styles.loginButton, styles.loginButtonPressedIn] : [styles.loginButton, styles.loginButtonPressedOut]}>
+           <Text style={styles.loginButtonText}>ShowPassword</Text>
+         </View>
+         </Pressable>
+        </View>
+      </View>
+      <View style={styles.loginContainer}>
+        <Pressable  onPressIn={()=>{setIsPressed(true)}} onPressOut={()=>{setIsPressed(false)}} onPress={() => alert("PRessed")}>
+         <View style={isPressed? [styles.loginButton, styles.loginButtonPressedIn] : [styles.loginButton, styles.loginButtonPressedOut]}>
+           <Text style={styles.loginButtonText}>Login</Text>
+         </View>
+         </Pressable>
+      </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  usernamePasswordContainer:{
+    flex:2,
+    backgroundColor:'green',
     justifyContent: 'center',
   },
+  usernameContainer:{
+    flex:1,
+    justifyContent:'center',
+    backgroundColor:'pink',
+    paddingHorizontal:20,
+  },
+  passwordContainer:{
+    flex:1,
+    justifyContent:'center',
+    backgroundColor:'gray',
+    paddingHorizontal:20,
+  },
+  username:{
+    backgroundColor:'white',
+    fontSize:25,
+    borderColor:'black',
+    borderWidth:2,
+    height:50,
+    borderRadius:15,
+  },
+  password:{
+    backgroundColor:'white',
+    fontSize:25,
+    borderColor:'black',
+    borderWidth:2,
+    height:50,
+    borderRadius:15,
+  },
+  label:{
+    fontSize: 20,
+    fontWeight:'bold',
+    margin:5,
+  },
+  loginContainer:{
+    flex:1, 
+    paddingHorizontal:25,
+  },
+  loginButton:{
+    margin: 5,
+    height: 50,
+    borderRadius:15,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  loginButtonText:{
+    color:'white',
+    fontSize:30,
+    fontWeight:'bold',
+  },
+  loginButtonPressedIn:{
+    backgroundColor:'red',
+  }
+  ,
+  loginButtonPressedOut:{
+    backgroundColor:'green',
+  }
 });
